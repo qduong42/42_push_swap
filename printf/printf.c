bdf15e1353d21271bcf7e267c41634d41754d51a
Quang huy Duong <qduong@c2s15d89.42wolfsburg.de>
2021-11-23 17:23:43 +0100

    Revert "all"
    This reverts commit 7ea0cebaeb4af9603dca67a0e6790c84cf1db3c1.

--- printf/printf.c
+++ printf/printf.c
@@ -3,14 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   printf.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qduong <qduong@student.42.fr>              +#+  +:+       +#+        */
+/*   By: qduong <qduong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 15:50:34 by qduong            #+#    #+#             */
-/*   Updated: 2021/11/23 16:53:52 by qduong           ###   ########.fr       */
+/*   Updated: 2021/10/24 21:52:01 by qduong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "printf.h"
+#include <unistd.h>
+#include <string.h>
+// #include <stdio.h>
 
 int	write_character(char c)
 {
@@ -18,79 +21,137 @@ int	write_character(char c)
 	return (1);
 }
 
-int	write_string(char *str)
+int write_string(char *str)
 {
-	int	i;
-
+	int i;
 	i = 0;
 	if (str == NULL)
-	{
-		write(1, "(null)", 6);
-		return (6);
-	}
-	while (*str)
+		{
+			write(1, "(null)", 6);
+			return(6);
+		}
+	while(*str)
 	{
 		write(1, str, 1);
 		str++;
 		i++;
 	}
-	return (i);
+	return(i);
 }
 
-int	write_pointer(unsigned long ptr)
+int write_pointer(unsigned long ptr)
 {
-	int	i;
-
+	int i;
 	i = 2;
 	write(1, "0x", 2);
-	if (ptr == 0)
-	{
-		write(1, "0", 1);
-		i++;
-	}
-	else
-		i = i + convert_hexa_lowercase(ptr);
-	return (i);
+	i = i + convert_hexa_lowercase(ptr);
+	return(i);
 }
-
-int	write_double(int d)
+int	convert_hexa_lowercase(unsigned long n)
 {
-	char	*str;
-	int		n;
-
-	n = 0;
-	str = ft_itoa(d);
-	ft_putstr_fd(str, 1);
-	n = ft_strlen(str);
-	free (str);
-	return (n);
+	int i;
+	i = 0;
+	if (n >= 16)
+	{
+		i = convert_hexa_lowercase((n / 16));
+		convert_hexa_lowercase((n % 16));
+	}
+	if (n <= 9)
+	{
+		write(1,'0' + n, 1);
+	}
+		
+	if (n >= 10 && n < 16)
+	{
+		write(1, 'a' - 10 + n, 1);
+	}
+	i++;
+	return(i);
 }
+
+int	write_double(double i)
+{
+	
+}
+
+int conversion(va_list args, char format)
+	{
+		if (format == 'c')
+			return(write_character(va_arg(args, int)));
+		if (format == 's')
+			return(write_string(va_arg(args, char *)));
+		if (format == 'p')
+			return(write_pointer(va_arg(args, unsigned long)));
+		if (format == 'd' || format == 'i')
+		return(write_double((va_arg(args, int)));
+		// if (format == 'x')
+		// 	return(write_low_hexa(va_arg(args, unsigned int));
+		// if (format == 'X')
+		// 	return(write_upper_hexa(va_arg(args, unsigned int)));
+		// if (format == 'u')
+		// 	return(write_unsigned_dec(va_arg(args, unsigned int)));
+		if (format == '%')
+		{
+		write(1, "\%", 1);
+		return (1);
+		}
+		return(0);
+		
+	}
 
 int	ft_printf(const char *format, ...)
 {
-	int			is_variable;
-	int			bytes_written;
-	va_list		args;
-
+	va_list args;
 	va_start (args, format);
+	int i;
+	int is_variable;
+	int bytes_written;
+	
+	i = 0;
 	is_variable = 0;
 	bytes_written = 0;
-	while (*format)
+	while (format[i]!= '\0')
 	{
 		if (is_variable)
+			{
+				bytes_written = conversion(args, format[i]);
+				is_variable = 0;
+				i++;
+			}
+		else if(format[i] != '%')
 		{
-			bytes_written += conversion(args, *format);
-			is_variable = 0;
-		}
-		else if (*format != '%')
-		{
-			write(1, format, 1);
+			write(1, &format[i], 1);
 			bytes_written++;
+			i++;
 		}
 		else
+		{
 			is_variable = 1;
-		format++;
+			i++;
+		}
 	}
-	va_end (args);
+	va_end(args);
 	return (bytes_written);
+}
+
+#include <stdio.h>
+# include "printf.h"
+#include <stdarg.h>
+
+int	main(void)
+{
+	int b = 5;
+	//char a[] = "Hello World";
+	int *p;
+	p = &b;
+	int c = 0;
+	//char c = 'a';
+	// //ft_printf("%s", a);
+	// printf("b-X-big:%X\nb-x-small:%x\n", b);
+	// printf("Hello, %s, %d, %X\n %p\n %d\n %x\n %X\n", a, b, c, p, p, p, p);
+	// c=ft_printf("abcd%s\n%c", a, c);
+	c=ft_printf("abcd%p\n",p);
+	
+	//printf("%d", c);
+	// print_ints(1, 5);
 }
